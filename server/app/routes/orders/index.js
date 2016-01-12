50824f00ed65f929a2f192e0be680a592d9a3fc9
Sam Narisi <snarisi@gmail.com>
2016-01-12 18:27:33 -0500

    Made route for adding to cart

--- server/app/routes/orders/index.js
+++ server/app/routes/orders/index.js
@@ -1,55 +1,45 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
+var Promise = require('bluebird');
 
 var Order = mongoose.model('Order');
+var User = mongoose.model('User');
+var Product = mongoose.model('Product');
 
-router.use('/cart', function(req, res, next) {
-    // if (req.user) {
-    //     req.user.findOrCreateCart()
-    //         .then(function(cart) {
-    //             req.cart = cart;
-    //             next();
-    //         },next);
-    // } else if (!req.session.cartId) {
-    //     Order.create({})
-    //         .then(function(order) {
-    //             req.session.cartId = order._id;
-    //             req.cart = order;
-    //             res.send('Created');
-    //             next();
-    //         },next);
-    // } else {
-    //     Order.findById(req.session.cartId)
-    //         .then(function(order) {
-    //         	req.cart = order;
-    //            	next();
-    //         },next);
-    // }
-    if (req.user) {
-        req.cart = req.user.cart
-    } else{
-    	req.session.cart = req.session.cart || [];
-    }
-
-});
 
 router.get('/', function(req, res, next) {
     res.status(200).send('Nothing here ya dang dingus');
 });
 
 router.get('/cart/addToCart/:id',function(req,res,next){
-	req.cart.addProduct(req.params.id)
-	.then(function(cart){
-		res.status(201).json(cart)
-	},next)
+    if (req.user) {
+        req.user.cart.push(req.params.id);
+        req.user.save()
+            .then(user => {
+                user.populate('cart', function (err, user) {
+                    res.status(201).json(user.cart);
+                });
+            })
+    } else {
+        req.session.cart = req.session.cart || [];
+        req.session.cart.push(req.params.id);
+        var promises = req.session.cart.map(productId => {
+            return Product.findById(productId);
+        });
+
+        Promise.all(promises)
+            .then(products => {
+                res.status(201).json(products);
+            })
+    }
 });
 
 router.get('/cart', function(req, res, next) {
-
-
-
-
-    res.status(200).json(req.cart);
+    if (req.user) {
+        res.status(200).send(req.user.cart);
+    } else {
+        res.status(200).send(req.session.cart);
+    }
 
 });
 
