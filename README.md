ab15fe01040f4531de8a02a8aa402b488cb24781
Joe Alves <joe@fullstackacademy.com>
2016-01-14 16:08:02 -0500

    Wipe commit

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Stories in Ready](https://badge.waffle.io/lenehan2/CatHats.png?label=ready&title=Ready)](https://waffle.io/lenehan2/CatHats)
--- server/db/models/index.js
+++
@@ -1,7 +0,0 @@
-// Require our models -- these should register the model into mongoose
-// so the rest of the application can simply call mongoose.model('User')
-// anywhere the User model needs to be used.
-require('./order');
-require('./product');
-require('./user');
-require('./review');
