5e1d18afe8156b494c89c8962baeb2ce2fb46d6d
Sam Narisi <snarisi@gmail.com>
2016-01-16 13:29:33 -0500

    Deleted findOrCreateCart method which we are no longer using

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -43,26 +43,6 @@ schema.methods.sanitize = function() {
     return _.omit(this.toJSON(), ['password', 'salt']);
 };
 
-schema.methods.findOrCreateCart = function(){
-    var self = this;
-
-    return this.populate('orders')
-    .then(function(user){
-        var cart = user.orders.find(function(order){
-            return !order.ordered;
-        })
-        if(cart){
-            return cart;
-        }else{
-            Order.create({})
-            .then(function(cart){
-                self.orders.push(cart._id);
-                return cart;
-            })
-        }
-    })
-}
-
 //addToCart can take either a single item or an array of items
 schema.methods.addToCart = function (newItems) {
     if (!Array.isArray(newItems)) newItems = [newItems];
