90e98312df2e91f8a068f645cabe0f142a76965a
Sam Narisi <snarisi@gmail.com>
2016-01-17 13:14:30 -0500

    Added contenteditable directive and editable functionaility on single product page

--- browser/js/product/productItem.directive.js
+++ browser/js/product/productItem.directive.js
@@ -1,4 +1,4 @@
-app.directive('productItem', function (CartFactory) {
+app.directive('productItem', function (CartFactory, ProductFactory, AuthService, $state) {
     return {
         restrict: 'E',
         scope: {
@@ -8,6 +8,27 @@ app.directive('productItem', function (CartFactory) {
         templateUrl: 'js/product/product-item.html',
         link: function (scope) {
             scope.addToCart = CartFactory.addProduct;
+
+            scope.isAdmin = false;
+            scope.editMode = false;
+
+            //TODO move this to the singleProduct controller
+            AuthService.getLoggedInUser()
+                .then(user => scope.isAdmin = user ? user.isAdmin : false);
+
+            scope.toggleEditMode = function () {
+                if (!scope.isAdmin) return;
+                scope.editMode = scope.editMode ? false : true;
+            }
+
+            scope.save = function (newData) {
+                ProductFactory.updateProduct(scope.product._id, newData)
+                    .then(function () {
+                        scope.toggleEditMode();
+                        $state.go('singleProduct', { productId: scope.product._id })
+                    });
+
+            }
         }
     }
-})
+});
