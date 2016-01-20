fd0668052592e964902f12eb69899501d1bf47c6
Sam Narisi <snarisi@gmail.com>
2016-01-20 10:34:08 -0500

    Categories on home page are now dynamic

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -13,6 +13,13 @@ app.config(function ($stateProvider) {
         controller: function($scope,featured, categories){
         	$scope.featured = featured;
             $scope.categories = categories;
+
+            $scope.pickCategory = function (categoryName) {
+                if (categoryName === 'Costumes') return 'fa fa-reddit-alien';
+                if (categoryName === 'Outerwear') return 'glyphicon glyphicon-tree-conifer';
+                if (categoryName === 'Formal') return 'fa fa-black-tie';
+                if (categoryName === 'Seasonal') return 'glyphicon glyphicon-tint';
+            }
         }
     });
 });
